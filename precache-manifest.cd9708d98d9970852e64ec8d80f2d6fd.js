4027ead22567828800f43747eaf8cf23fa981588
grantimusPrime <700gtkeller@gmail.com>
2020-09-04 17:40:16 -0600

    Updates

---
+++ precache-manifest.cd9708d98d9970852e64ec8d80f2d6fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f3b4dd4ce549087b0f556cfc7df1a5",
+    "url": "/chris-double-d6/index.html"
+  },
+  {
+    "revision": "30ec33ce772aa59cc084",
+    "url": "/chris-double-d6/static/css/main.e868fe69.chunk.css"
+  },
+  {
+    "revision": "2ed153bdf183682ea9de",
+    "url": "/chris-double-d6/static/js/2.330ed6fb.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/chris-double-d6/static/js/2.330ed6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ec33ce772aa59cc084",
+    "url": "/chris-double-d6/static/js/main.48678dd9.chunk.js"
+  },
+  {
+    "revision": "c1ead347b0b4454aa524",
+    "url": "/chris-double-d6/static/js/runtime-main.60343c63.js"
+  }
+]);
